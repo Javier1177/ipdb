888c942f37af044b015732c7432516d3f40826ab
Javi <javierlopezblasco@gmail.com>
2022-07-04 14:00:21 +0200

    Add Lato font, modify some layout, searchbox and welcome styles

--- src/components/welcome/welcome.component.jsx
+++ src/components/welcome/welcome.component.jsx
@@ -5,14 +5,16 @@ import './welcome.styles.scss';
 const Welcome = () => {
   return (
     <section className='welcome-container'>
-      <span>Welcome to</span>
-      <h1 className='welcome-title'>Wiki Pokemon</h1>
-      <p>
-        Here you can find information about any pokemon by its name. <br />
-        This web uses pokemon API "pokeapi" to get the pokemon data and show it
-        in a friendly way to you. <br />
-        Search any pokemon you want or see one of our suggested pokemons.
-      </p>
+      <div className='welcome-text-section'>
+        <span>Welcome to</span>
+        <h1 className='welcome-title'>Wiki Pokemon</h1>
+        <p>
+          Here you can find information about any pokemon by its name. <br />
+          This web uses pokemon API "pokeapi" to get the pokemon data and show
+          it in a friendly way to you. <br />
+          Search any pokemon you want or see one of our suggested pokemons.
+        </p>
+      </div>
       <SearchBox />
     </section>
   );
